f3fbe6a261eed7a17dcae6a22e329d3fcd43c0ab
wahyuWs <wahyusantozo36@gmail.com>
2023-08-28 13:26:16 +0700

    create function radio button

--- app/src/main/java/com/wahyus/ytmp3example/MainActivity.kt
+++ app/src/main/java/com/wahyus/ytmp3example/MainActivity.kt
@@ -2,10 +2,48 @@ package com.wahyus.ytmp3example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import com.wahyus.ytmp3example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainActivityBinding: ActivityMainBinding
+    private var qualityMusic = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainActivityBinding.root)
+
+        mainActivityBinding.btnSearch.setOnClickListener {
+            //call api on viewmodel
+        }
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when(view.id) {
+                R.id.rb_64 -> if (checked) {
+                    qualityMusic = 64
+                }
+                R.id.rb_128 -> if (checked) {
+                    qualityMusic = 128
+                }
+                R.id.rb_256 -> if (checked) {
+                    qualityMusic = 256
+                }
+                R.id.rb_192 -> if (checked) {
+                    qualityMusic = 192
+                }
+                R.id.rb_320 -> if (checked) {
+                    qualityMusic = 320
+                }
+            }
+        }
     }
 }
